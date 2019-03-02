673ec3b9154620aae915e489dfa1da00ab51fee5
Jordan Babbitt <jordanbabbitt@Jordans-MacBook-Pro.local>
2019-03-01 23:06:57 -0800

    I cannot figure out why on earth my html wont connect to my game.js file. I have tried everything i can imagine including googling the world. Everything say's ive done things correctly. Please help me when you grade it to find my error. Thank you.

--- game.js
+++ game.js
@@ -9,27 +9,27 @@ var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.l
 var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 
 
-document.onkeyup = function(event) {
+document.onkeyup = function (event) { }
 
 if (options.indexOf(userGuess) > -1) {
 
-if (userGuess === computerGuess) {
+	if (userGuess === computerGuess) {
 		wins++;
 		numberOfGuesses = 9;
 		guessOptions = [];
-}
+	}
 
-if (userGuess != computerGuess) {
-	numberOGuesses --;
-	guessOptions.push(userGuess);
-}
+	if (userGuess != computerGuess) {
+		numberOGuesses--;
+		guessOptions.push(userGuess);
+	}
 
-if (numGuesses === 0) {
+	if (numGuesses === 0) {
 
-	numberOGuesses = 9;
-	losses ++;
-	guessOptions = [];
+		numberOGuesses = 9;
+		losses++;
+		guessOptions = [];
 
-				
-}
+
+	}
 }
